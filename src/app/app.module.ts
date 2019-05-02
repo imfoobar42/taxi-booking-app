5fb587e64e3d5d83ab71e78135d5cfd985a6db64
Singh <akash.m.singh@capgemini.com>
2019-05-02 13:04:28 +0530

    Routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DriverModule } from 'src/driver/driver.module';
+import { UserModule } from './user/user.module';
+import { RouterModule } from '@angular/router';
+import { appRouter } from './app.router';
+import { WelcomeComponent } from './welcome.component';
+import { ResourceNotFoundComponent } from './notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ResourceNotFoundComponent
+    
   ],
   imports: [
     BrowserModule,
-    DriverModule
+    UserModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/driver/driver-home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-templateUrl:"./driver-home.component.html",
-selector:"driver-home"
-})
-export class DriverHomeComponent{
-
-}
